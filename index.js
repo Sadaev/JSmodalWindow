5ad214d83e40cfcb3f12f960cc4434e8b90c8f4e
Садаев Шынгыс <sadaevsh@gmail.com>
2021-02-14 15:13:04 +0600

    lesson 2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const modal = $.modal()
+const fruits = [
+    {id: 1, title: 'Яблоки', price: 20},
+    {id: 2, title: 'Апельсин', price: 30},
+    {id: 3, title: 'Манго', price: 40}
+]
+/*
+ *1. Динамичемки создавать карточки фруктов
+ *2.Покозать цену в модалке (и это должна быть одна модалка) 
+ *3.Модалка для удаления с 2 кнопками
+ *----------------
+ *4. Динамически удаление карточки. На основе плагина модуль сделать плагин $.confirm(Promise)
+ **/
+const modal = $.modal({
+    width : '400px',
+    closable : true,
+    title : 'Sadaev Modal',
+    content : `
+        <h4>Modal is workin</h4>
+        <p>This is body by mo</p>
+        <p>This is body by mo</p>
+    `,
+    footerButtons: [
+        {
+            text: 'Ok', 
+            type: 'primary', 
+            handler(){
+                console.log('Primery btn clicked');
+                modal.close()
+            }
+        },
+        {
+            text: 'Cancel', 
+            type: 'danger', 
+            handler(){
+            console.log('Danger btn clicked');
+            modal.close()
+            }
+        }
+    ]
+})
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,56 +1,88 @@
-function _createModal(options){
+Element.prototype.appendAfter = function(element) {
+    element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []){
+    if(buttons.length === 0){
+        return document.createElement('div')
+    }
+
+    const wrap = document.createElement('div')
+    wrap.classList.add('modal-footer')
+
+    buttons.forEach(btn => {
+        const $btn = document.createElement('button')
+        $btn.textContent = btn.text
+        $btn.classList.add('btn')
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        $btn.onclick = btn.handler || noop
+
+        wrap.appendChild($btn)
+    })
+
+    return wrap
+}
+
+
+
+
+function _createModal({title = 'Modal', closable = true, content = '', width = '600px', footerButtons}){
     const modal = document.createElement('div')
     modal.classList.add('smodal')
     modal.insertAdjacentHTML('afterbegin', `
-    <div class="modal-overlay">
-        <div class="modal-window">
+    <div class="modal-overlay" data-close = "true">
+        <div class="modal-window" style ="width: ${width} !important">
             <div class="modal-header">
-                <span class="modal-title">Modal title</span>
-                <span class="modal-close">&times;</span>
+                <span class="modal-title">${title}</span>
+                ${closable ? `<span class="modal-close" data-close = "true">&times;</span>` : ``}
             </div>
-            <div class="modal-body">
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum dolor sit.</p>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-success">Ok</button>
-                <button class="btn btn-warning">Cancel</button>
+            <div class="modal-body" data-content>
+                ${content}
             </div>
         </div>
     </div>
     `)
+    const footer = _createModalFooter(footerButtons)
+    footer.appendAfter(modal.querySelector('[data-content]'))
     document.body.appendChild(modal)
     return modal
 }
 
 /**
  * 
- * title: string
- * closable: boolean
- * content: string  (format html dinamic content)
- * width: string ('400px')
- * destroy(){}: void
+ * title: string+
+ * closable: boolean+
+ * content: string  (format html dinamic content)+
+ * width: string ('400px')+
+ * destroy(){}: void+
  * 
- * close modal window when press on overlay or cross
+ * close modal window when press on overlay or cross+
  * -------------------------
- *public method for instanse setContent(html: string): void 
+ *public method for instanse setContent(html: string): void +
+ *
  dynamykacl change contain of this modal
- *public method liveCicle hooks
+ *public method liveCicle hooks:
  *onClose() : void
  *onOpen(): void
  *beforeClose() : boolean
- *---------------
+ *--------------------------
  *Animate Css animate.css
  *use library animate.css
-
+*/
 
 $.modal  = function (options) {
     const ANIMATION_SPEED = 200
     const $modal = _createModal(options)
     let closing = false
+    let destroyed = false
 
-    return {
+    const modal = {
         open(){
+            if(destroyed){
+                return console.log('Modal is destroyed')
+            }
             !closing && $modal.classList.add('open')
         },
         close(){
@@ -62,6 +94,24 @@ $.modal  = function (options) {
                 closing = false
             }, ANIMATION_SPEED)
         },
-        destroy(){}        
     }
+
+    const listener = event => {
+        if(event.target.dataset.close){
+            modal.close()
+        }
+    }
+
+    $modal.addEventListener('click', listener)
+
+    return Object.assign(modal, {
+        destroy() {
+            $modal.parentNode.removeChild($modal) // delete in html
+            $modal.removeEventListener('click', listener) // delet listeneres befor safe listener in cach
+            destroyed = true
+        },
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML = html
+        }
+    })
 }
